c9b0f9cc7480efbb6b27f79b8fcd66b6ca3ee3ac
Michael <Admin@Admins-Mac-mini.attlocal.net>
2014-12-09 12:33:03 -0600

    added a test class

---
+++ Git test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git test
+//
+//  Created by Michael on 12/9/14.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git test
+//
+//  Created by Michael on 12/9/14.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
