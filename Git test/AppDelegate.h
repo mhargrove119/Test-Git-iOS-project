9dab3255624db4bf2ed8b9ff2297b23611165cb3
Michael <Admin@Admins-Mac-mini.attlocal.net>
2014-12-09 12:17:19 -0600

    Initial Commit

---
+++ Git test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git test
+//
+//  Created by Michael on 12/9/14.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git test
+//
+//  Created by Michael on 12/9/14.
+//  Copyright (c) 2014 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
